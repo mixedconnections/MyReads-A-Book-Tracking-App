475a69c2cf2a8c6aae5ea8ffb229ed303b63fad2
Mike Accardo <perlhero@yahoo.com>
2018-02-15 13:50:39 -0500

    Added filter to bookArray

--- src/components/BookCategories.js
+++ src/components/BookCategories.js
@@ -7,11 +7,11 @@ class BookCategories extends Component {
                    return <div className="list-books">
 			     <div className="list-books-content">
 				<div>
-				   <BookCategory shelfTitle='Currently Reading' bookArray={this.props.currentlyReading}
+				   <BookCategory shelfTitle='Currently Reading' bookArray={this.props.books.filter(book => book.shelf === 'currentlyReading')}
 			                            onCategoryChange={this.props.onCategoryChange}/>,
-				   <BookCategory shelfTitle='Want to Read' bookArray={this.props.wantToRead}
+				   <BookCategory shelfTitle='Want to Read' bookArray={this.props.books.filter(book => book.shelf === 'wantToRead')}
 			      			    onCategoryChange={this.props.onCategoryChange}/>,
-				   <BookCategory shelfTitle='Read' bookArray={this.props.read}
+				   <BookCategory shelfTitle='Read' bookArray={this.props.books.filter(book => book.shelf === 'read')}
 			                            onCategoryChange={this.props.onCategoryChange}/>
 			         </div>
 			     </div>  
